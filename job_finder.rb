8756ed8f7954cfcd4f4ce0a4a190db221ee99724
Theo Gieles <theogieles@gmail.com>
2017-04-24 14:51:36 -0400

    The first version of my job finder

---
+++ job_finder.rb
@@ -0,0 +1,16 @@
+require 'httparty'
+require 'nokogiri'
+
+url = "https://miami.craigslist.org/search/sof"
+
+response = HTTParty.get url
+
+dom = Nokogiri::HTML(response.body)
+
+job_titles =
+
+	dom.css('.hdrlnk').map do |obj|
+		obj.content
+	end
+
+p job_titles
